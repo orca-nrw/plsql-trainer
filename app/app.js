fad35515243a47eb50aa60b9c39c57c8a7e9de78
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-05-09 19:50:25 +0200

    Initial Project-Setup

---
+++ app/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+/*
+    Server Configuration
+ */
+
+let path = __dirname + '/views/'
+
+// Use pug
+app.set('view engine', 'pug')
+
+// Handle static css and js files automatically
+app.use(express.static(__dirname))
+
+/*
+Application Code
+*/
+app.get("/", async (req, res) => {
+    res.send("Hello World!")
+})
+
+// Start server
+app.listen(8080, () => {
+    console.log(`Server listening at http://localhost:8080/`)
+})
+
+// Handle Shutdown
+process.on('SIGINT', () => {
+    console.log('Exiting application')
+    process.exit(0)
+})
