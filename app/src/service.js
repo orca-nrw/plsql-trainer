63c8505d113fb5d08ff4b02cbeeadabe872a2e11
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-05-25 12:17:32 +0200

    Added service file to handle global data like table information

--- app/src/service.js
+++ app/src/service.js
@@ -1,1 +1,23 @@
 const db = require('./database')
+
+/**
+ * typedef {{headers: string[], rows: *[] }} Table
+ */
+
+/**
+ * Wrapper to turn DatabaseResponse into Table
+ * @param {string} tableName
+ * @returns {Table}
+ */
+async function getFormattedTable(tableName) {
+    let rawTables = await db.getTable(tableName)
+    let table = {
+        headers: rawTables.metaData.map(meta => meta.name),
+        rows: rawTables.rows
+    }
+    return table
+}
+
+module.exports = {
+    getFormattedTable
+}
