df6c93578b01920bc0f9d46dadcc2df66d55f5dd
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-06-13 15:40:06 +0200

    Added question post to support redirects back

--- app/src/trigger/router.js
+++ app/src/trigger/router.js
@@ -35,6 +35,19 @@ router.get('/question/:id', async (req, res, next) => {
     }
 })
 
+router.post('/question/:id', async (req, res, next) => {
+    try {
+        let variables = {
+            question: await service.getQuestion(req.params.id)
+        }
+        variables['test_trigger'] = req.body['test_trigger']
+        
+        res.render(path + 'question', variables)
+    } catch (err) {
+        next(err)
+    }
+})
+
 router.post('/evaluation', async (req, res, next) => {
     let questionId = req.body['question_id']
     let testTrigger = req.body['test_trigger']
