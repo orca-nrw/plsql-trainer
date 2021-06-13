1d4fdc517ac61c6ada064fccbd4a4df42327591b
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-06-13 15:40:31 +0200

    First draft of evaluation page

--- app/src/trigger/router.js
+++ app/src/trigger/router.js
@@ -49,20 +49,34 @@ router.post('/question/:id', async (req, res, next) => {
 })
 
 router.post('/evaluation', async (req, res, next) => {
-    let questionId = req.body['question_id']
-    let testTrigger = req.body['test_trigger']
-    let neededTables = req.body['needed_tables']
+    try {
+        let variables = {}
 
-    // Try-Catch might be more elegant?
-    let validationResults = validator.validateTrigger(testTrigger, neededTables)
-    if (!validationResults.isValid) {
-        
-    } else {
-        variables['test_success'] = false
-        variables['test_result'] = validationResults.errorMessage
+        let questionId = req.body['question_id']
+        let testTrigger = req.body['test_trigger']
+        let neededTables = req.body['needed_tables']
+
+        variables['question_id'] = questionId
+        variables['test_trigger'] = testTrigger
+    
+        // Try-Catch might be more elegant?
+        let validationResults = validator.validateTrigger(testTrigger, neededTables)
+        if (!validationResults.isValid) {
+            let triggerEvaluation = await service.evaluateTrigger(questionId, testTrigger)
+            let firingStatements = await service.getFiringStatements(questionId)
+
+            variables['evaluation'] = triggerEvaluation
+            variables['test_success'] = true
+            variables['firing_statements'] = firingStatements
+        } else {
+            variables['test_success'] = false
+            variables['test_result'] = validationResults.errorMessage
+        }
+        res.render(path + 'evaluation', variables)
+    } catch (err) {
+        next(err)
     }
-
-    res.render(path + 'evaluation', variables)
+    
 })
 
 module.exports = router
