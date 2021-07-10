79eb21e03312dd078cae9d2fb6598041b57c37a1
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-10 21:34:01 +0200

    Added evaluation queries

--- app/src/functions/database.js
+++ app/src/functions/database.js
@@ -36,7 +36,59 @@ async function getRawFuncQuestion (questionId) {
   return result
 }
 
+/**
+ * Use package-function evaluate_question to evaluate the function written by the user
+ * @param {number} questionId
+ * @param {string} userFunction
+ * @returns {DatabaseResponse}
+ */
+async function getRawFunctionEvaluation (questionId, userFunction) {
+  const connection = await oracledb.getConnection(dbconfig)
+  const result = await connection.execute(
+        `BEGIN
+            :cursor := edb_plsql_func_app.evaluate_question(:question_id, :user_function, :identifier_key);
+        END;`,
+        {
+          question_id: questionId,
+          user_function: userFunction,
+          identifier_key: 'Test',
+          cursor: { dir: oracledb.BIND_OUT, type: oracledb.CURSOR }
+        }
+  )
+  const resultSet = result.outBinds.cursor
+
+  const metaData = resultSet.metaData
+  const rows = await resultSet.getRows(1)
+
+  await connection.close()
+
+  return { metaData: metaData, rows: rows }
+}
+
+/**
+ * Query the database for all statements used to test the function
+ * @param {number} questionId
+ * @returns {DatabaseResponse}
+ */
+async function getRawTestCalls (questionId) {
+  const connection = await oracledb.getConnection(dbconfig)
+  const result = await connection.execute(
+        `SELECT CALL
+        FROM FUNC_CALLS
+        WHERE QUESTIONID = :question_id
+        ORDER BY EXECORDER ASC`,
+        {
+          question_id: questionId
+        }
+  )
+  await connection.close()
+
+  return result
+}
+
 module.exports = {
   getRawFuncQuestions,
-  getRawFuncQuestion
+  getRawFuncQuestion,
+  getRawFunctionEvaluation,
+  getRawTestCalls
 }
