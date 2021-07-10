234f52ee149b3a294ab2e1859dcb719618a35bd4
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-10 20:05:10 +0200

    Updated import path

--- app/src/trigger/router.js
+++ app/src/trigger/router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const service = require('./service')
-const validator = require('./validator')
+const validator = require('../util/validator')
 const path = require('path')
 
 /*
