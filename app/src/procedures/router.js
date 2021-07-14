0b3ed7e17efb8065998424f878fa3515b9b35ae2
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-14 18:24:33 +0200

    Added method placeholders

---
+++ app/src/procedures/router.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const service = require('./service')
+const validator = require('../util/validator')
+
+/*
+    Server Configuration
+ */
+
+const viewPath = path.join(__dirname, 'views')
+
+router.get('/', async (req, res) => {
+  res.render(path.join(viewPath, 'functions'), {})
+})
+
+/*
+    Routes
+*/
+
+router.get('/', async (req, res) => {
+  res.render(path.join(viewPath, 'procedures'))
+})
+
+router.get('/questions', async (req, res, next) => {
+  try {
+    const variables = {
+      // TODO
+    }
+    res.render(path.join(viewPath, 'questions'), variables)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/question/:id', async (req, res, next) => {
+  try {
+    const variables = {
+      // TODO
+    }
+    res.render(path.join(viewPath, 'question'), variables)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/question/:id', async (req, res, next) => {
+  try {
+    const variables = {
+      // TODO
+    }
+    res.render(path.join(viewPath, 'question'), variables)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/evaluation', async (req, res, next) => {
+  try {
+    const variables = {}
+
+    // TODO
+
+    res.render(path.join(viewPath, 'evaluation'), variables)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
